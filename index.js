be9be13c3122d97442bc5348ae3388f3ec8d0937
Gabriel dias <gds9@aluno.ifal.edu.br>
2025-10-14 22:09:43 -0300

    feat: Commit inicial do projeto com arquivos HTML e CSS

---
+++ index.js
@@ -0,0 +1,16 @@
+/*let botao = document.getElementById("tela_inicio");
+botao = document.addEventListener("onclick", () => {
+   location.href("tela_login.html")
+})*/
+
+// criando uma variavel para o botao 
+/*const botao = document.getElementById("tela_inicio");
+
+if (botao) {
+    botao.addEventListener("click", () => {
+
+        location.href = "tela_login.html"
+    })
+}*/
+
+
